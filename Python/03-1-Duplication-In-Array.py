be8051f6842a48e2a51c9c2e54a40b2c76443fe7
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-06-07 16:24:24 -0700

    Rename 03

--- Python/03-1-Duplication-In-Array.py
+++ Python/03-1-Duplication-In-Array.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+面试题3（一）：找出数组中重复的数字
+题目：在一个长度为n的数组里的所有数字都在0到n-1的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
+也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。例如，如果输入长度为7的数组{2, 3, 1, 0, 2, 5, 3}，
+那么对应的输出是重复的数字2或者3。
+'''
+
+
 # Solution 1: Using Counter
 # Time & Space: O(n)
 from collections import Counter
@@ -10,7 +20,7 @@ class Solution1(object):
 
         count = Counter( nums )
 
-        return [ num for num in count if count[ num ] > 1 ]
+        return [ num for num in count if count[ num ] > 1 ][0]
 
 
 # Solution 2: Hash Table
@@ -27,9 +37,8 @@ class Solution2(object):
             if num not in hashMap:
                 hashMap[ num ] = 1
             else:
-                hashMap[ num ] += 1
+                return num
 
-        return [ key for key in hashMap if hashMap[ key ] > 1 ]
 
 # Solution 3: Sorting
 # Time: O(nlogn); Space: O(1)
@@ -40,16 +49,12 @@ class Solution3(object):
         :rtype: List[int]
         """
         nums.sort( reverse = False )
-        ans = []
         i = 0
         while i < len(nums):
             if i + 1 < len(nums) and nums[i] == nums[i+1]:
-                ans.append( nums[i])
-                while nums[i] == nums[i+1]:
-                    i += 1
+                return nums[i]
             i += 1
 
-        return ans
 
 # Solution 4: Sort + Compare
 # Time: O(n); Space: O(1)
@@ -61,17 +66,16 @@ class Solution4(object):
         """
         n = len( nums )
         i = 0
-        ans = []
         while i < n:
             if nums[ nums[i] ] != nums[i]:
                 nums[ nums[i] ], nums[i] = nums[i], nums[ nums[i] ]
                 continue
             elif nums[ nums[i] ] == nums[i] and i != nums[i]:
-                ans.append( nums[i] )
+                return nums[i]
             
             i += 1
 
-        return ans
+
 
 S = Solution4()
 print S.duplicationInArray( [2,3,1,0,2,5,3] )
