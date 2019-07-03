67c0dc65475ef41e5c4edad5533c552a3854b2f0
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-07-02 17:26:04 -0700

    Create 25-Merge-Sorted-Lists.py

---
+++ Python/25-Merge-Sorted-Lists.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+'''
+// 面试题25：合并两个排序的链表
+// 题目：输入两个递增排序的链表，合并这两个链表并使新链表中的结点仍然是按
+// 照递增排序的。例如输入图3.11中的链表1和链表2，则合并之后的升序链表如链
+// 表3所示。
+
+同LeetCode 21: https://leetcode.com/problems/merge-two-sorted-lists/
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode( None )
+        p = head
+        
+        p1, p2 = l1, l2
+        while p1 and p2:
+            if p1.val <= p2.val:
+                p.next = p1
+                p1 = p1.next
+            else:
+                p.next = p2
+                p2 = p2.next
+            p = p.next
+        
+        if p1:
+            p.next = p1
+        
+        if p2:
+            p.next = p2
+        
+        return head.next
+            
